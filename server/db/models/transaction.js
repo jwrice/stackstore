0dd34ca89c561a98c146e12426533bed028fc5dc
Christian Ayscue <coayscue@gmail.com>
2015-07-28 10:48:09 -0400

    user routes done

--- server/db/models/transaction.js
+++ server/db/models/transaction.js
@@ -4,16 +4,11 @@ var mongoose = require("mongoose");
 
 var transaction = new mongoose.Schema({
   user: {
-<<<<<<< HEAD
     type: mongoose.Schema.Types.ObjectId,
-=======
-    type: mongoose.Schema.Types.ObjectId, 
->>>>>>> master
     ref: "User",
     required: true
   },
   product: {
-<<<<<<< HEAD
     type: mongoose.Schema.Types.ObjectId,
     ref: "Product",
     required: true
@@ -21,10 +16,6 @@ var transaction = new mongoose.Schema({
   instructor: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "Instructor",
-=======
-    type: mongoose.Schema.Types.ObjectId, 
-    ref: "Product",
->>>>>>> master
     required: true
   },
   time: {
