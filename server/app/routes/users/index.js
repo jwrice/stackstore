e0d7633f383a79266de306b4feb04bd6216ed446
Xavier Durand-Smet <xavier.durand-smet>
2015-07-28 17:08:24 -0400

    routes for users done

--- server/app/routes/users/index.js
+++ server/app/routes/users/index.js
@@ -27,25 +27,14 @@ router.get('/:userId', function(req, res, next) {
 // adding/deleting products to the cart
 // changing info on user account page
 router.put('/:userId', function(req, res, next) {
-	console.log('req.body', req.body)
-	// User.update({_id: req.currentUser._id}, {$set: req.body})
-	// .then(function(user){
-	// 	console.log(user);
-	// 	res.json(user);
-	// })
 	User.findByIdAndUpdate(req.currentUser._id, req.body, {'new': true})
 	.then(function(user) {
-		console.log('user', user)
 			if (!user) throw new Error("user not found");
 			req.currentUser = user;
 			res.json(user);
 			next();
 		})
 		.then(null, next);
-	// req.currentUser = req.body;
-	// req.currentUser.save();
-	// // console.log('req.currentUser', req.currentUser)
-	// res.json(req.currentUser);
 });
 
 // sign up
--- tests/server/routes/user-routes.js
+++ tests/server/routes/user-routes.js
@@ -1,103 +1,92 @@
-// // Instantiate all models
-// var mongoose = require('mongoose');
-// require('../../../server/db/models');
-// var User = mongoose.model('User');
+// Instantiate all models
+var mongoose = require('mongoose');
+require('../../../server/db/models');
+var User = mongoose.model('User');
 
-// var expect = require('chai').expect;
+var expect = require('chai').expect;
 
-// var dbURI = 'mongodb://localhost:27017/testingDB';
-// var clearDB = require('mocha-mongoose')(dbURI);
+var dbURI = 'mongodb://localhost:27017/testingDB';
+var clearDB = require('mocha-mongoose')(dbURI);
 
-// var supertest = require('supertest');
-// var app = require('../../../server/app');
-// var User = mongoose.model('User');
-
-
-// describe('Users Route', function () {
-
-// 	beforeEach('Establish DB connection', function (done) {
-// 		if (mongoose.connection.db) return done();
-// 		mongoose.connect(dbURI, done);
-// 	});
-
-// 	afterEach('Clear test database', function (done) {
-// 		clearDB(done);
-// 	});
-
-// 	// describe('Unauthenticated request', function () {
-
-// 	// 	var guestAgent;
-
-// 	// 	beforeEach('Create guest agent', function () {
-// 	// 		guestAgent = supertest.agent(app);
-// 	// 	});
-
-// 	// 	it('should get a 401 response', function (done) {
-// 	// 		guestAgent.get('/api/members/secret-stash')
-// 	// 			.expect(401)
-// 	// 			.end(done);
-// 	// 	});
+var supertest = require('supertest');
+var app = require('../../../server/app');
+var User = mongoose.model('User');
 
 
-// // OUR CODE 
+describe('Users Route', function () {
 
-// 		describe('Requests to /users', function () {
+	beforeEach('Establish DB connection', function (done) {
+			if (mongoose.connection.db) return done();
+			mongoose.connect(dbURI, done);
+		});
 
-// 		var guestAgent;
+	afterEach('Clear test database', function (done) {
+		clearDB(done);
+	});
 
-// 		beforeEach('Create guest agent', function () {
-// 			guestAgent = supertest.agent(app);
-// 			var userId;
-// 			return User.create({
-// 				firstName: 'Gavin',
-// 				lastName: 'X',
-//                 email : 'user@gmail.com',
-//                 password: '123',
-//                 cart: [],
-//                 salt: '123',
-//                 pastPurchases: []
-//             })
-//             .then(function(user) {
-//             	userId = user._id;
-//             	return userId;
-//             })
-// 		});
+	describe('Requests to /users', function () {
 
-// 		it('should get a 200 response', function (done) {
-// 			guestAgent.get('/users/userId')
-// 				.expect(200)
-// 				.expect(response.body.firstName).to.be('Gavin')
-// 				.end(done);
-// 		});
+		var guestAgent;
+		var userId;
 
-// 	});
+		beforeEach('Create guest agent', function () {
+			guestAgent = supertest.agent(app);
+			return User.create({
 
-// 	describe('Authenticated request', function () {
+				firstName: 'Gavin',
+				lastName: 'X',
+                email : 'user@gmail.com',
+                password: '123',
+                cart: [],
+                salt: '123',
+                pastPurchases: []
+            })
+            .then(function(user) {
+            	userId = user._id;
+            	return userId;
+            })
+		});
 
-// 		var loggedInAgent;
+		it('should get the user info', function (done) {
+			guestAgent.get('/api/users/'+userId)
+				.expect(200)
+				.end(function(err, res){
+					if(err) return done(err);
+					expect(res.body.lastName).to.equal('X');
+					done();
+				})
+		});
 
-// 		var userInfo = {
-// 			email: 'joe@gmail.com',
-// 			password: 'shoopdawoop'
-// 		};
+		it('should create a new user', function(done){
+			guestAgent.post('/api/users')
+			.send({
+				firstName: 'Xavier',
+				lastName: 'Y',
+                email : 'Xavier@gmail.com',
+                password: '12345',
+                cart: [],
+                salt: '12312',
+                pastPurchases: []
+			}).end(function(err, res){
+				// console.log(res.body);
+				expect(res.body.firstName).to.equal("Xavier");
+				done();
+			})
+		})
 
-// 		beforeEach('Create a user', function (done) {
-// 			User.create(userInfo, done);
-// 		});
+		it('should update a user', function(done){
+			guestAgent.put('/api/users/' + userId)
+			.send({
+				firstName: 'UpdatedName',
+			}).end(function(err, res){
+				// console.log(res.body);
+				expect(res.body.firstName).to.equal("UpdatedName");
+				done();
+			})
+		})
 
-// 		beforeEach('Create loggedIn user agent and authenticate', function (done) {
-// 			loggedInAgent = supertest.agent(app);
-// 			loggedInAgent.post('/login').send(userInfo).end(done);
-// 		});
 
-// 		it('should get with 200 response and with an array as the body', function (done) {
-// 			loggedInAgent.get('/api/members/secret-stash').expect(200).end(function (err, response) {
-// 				if (err) return done(err);
-// 				expect(response.body).to.be.an('array');
-// 				done();
-// 			});
-// 		});
+	});
 
-// 	});
 
-// });
+})
